f90a8ff9d1042471f9d18ec3aeb93a7c68fe230e
ganeshnishad707166-code <ganeshnishad707166@gmail.com>
2026-06-30 15:27:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector("button").addEventListener("click", function () {
+
+    const upi = "upi://pay?pa=8085824525@ybl&pn=VIP%20Earbuds&am=10&cu=INR";
+
+    window.location.href = upi;
+
+});
